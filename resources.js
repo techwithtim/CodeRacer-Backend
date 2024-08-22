3e8811130c8620ff739922a092ddc4845d4750a0
Tim Ruscica <techwithtimm@gmail.com>
2024-08-22 11:20:49 +0800

    backend changes

--- resources.js
+++ resources.js
@@ -1,5 +1,6 @@
 export class Scores extends tables.Score {
-	
+	allowRead() { return true }
+
 	post(content) {
 		content.region = server.config.clustering.nodeName
 		console.log(content, server.config.clustering)
